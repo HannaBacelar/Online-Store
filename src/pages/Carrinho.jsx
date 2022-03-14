d2e80bb35d7746c178d40b83e5a92b2966a9218d
joao_victor_leal <joaovictorgusmaoleal@hotmail.com>
2022-03-14 20:12:02 -0300

    requisito 8 feito

--- src/pages/Carrinho.jsx
+++ src/pages/Carrinho.jsx
@@ -1,9 +1,49 @@
 import React from 'react';
 
 class Carrinho extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      cartItems: [],
+    };
+  }
+
+  componentDidMount() {
+    this.getCartItems();
+  }
+
+  getCartItems = () => {
+    const values = Object.values(localStorage);
+    // const teste = localStorage.getItem(values);
+    // console.log(teste);
+    this.setState({
+      cartItems: values,
+    });
+  }
+
   render() {
+    const { cartItems } = this.state;
     return (
-      <p data-testid="shopping-cart-empty-message">Seu carrinho está vazio</p>
+      <>
+        {
+          cartItems.length <= 0
+            ? <p data-testid="shopping-cart-empty-message">Seu carrinho está vazio</p>
+            : cartItems.map((item, index) => (
+              <div key={ index }>
+                <p data-testid="shopping-cart-product-name">
+                  { item }
+                </p>
+              </div>
+            ))
+        }
+
+        <p
+          data-testid="shopping-cart-product-quantity"
+        >
+          {`Quantidade de itens: ${cartItems.length}`}
+        </p>
+      </>
     );
   }
 }
